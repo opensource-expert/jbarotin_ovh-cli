18a3c8ad2ad5d76edecdb2abb04b7ea7c0dee82d
Sylvain Viart <sylvain303@github>
2020-03-18 05:45:27 +0100

    use .get() to fetch key in get_general

--- ovh_cli/secret.py
+++ ovh_cli/secret.py
@@ -18,20 +18,20 @@ def get_config(key):
 def get_general(key="general"):
     config = get_config(key)
     return {
-        "project_name": config[key]['project_name'],
-        "base_distrib": config[key]['base_distrib'],
-        "region": config[key]['region'],
-        "ssh_key_id": config[key]['ssh_key_id'],
-        "inventary_file": config[key]['inventary_file'],
-        "private_network_id": config[key]['private_network_id'],
-        "public_network_id": config[key]['public_network_id']
+        "project_name": config[key].get('project_name'),
+        "base_distrib": config[key].get('base_distrib'),
+        "region": config[key].get('region'),
+        "ssh_key_id": config[key].get('ssh_key_id'),
+        "inventary_file": config[key].get('inventary_file'),
+        "private_network_id": config[key].get('private_network_id'),
+        "public_network_id": config[key].get('public_network_id')
     }
 
 
 def get_ovh_connection_setting(key="get-info"):
     config = get_config(key)
     return {
-        "application_key": config[key]['application_key'],
-        "application_secret": config[key]['application_secret'],
-        "consumer_key": config[key]['consumer_key']
+        "application_key": config[key].get('application_key'),
+        "application_secret": config[key].get('application_secret'),
+        "consumer_key": config[key].get('consumer_key')
     }
